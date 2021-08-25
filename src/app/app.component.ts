887d99460e95549933a17c5976cdb78f58889a4e
VenomRishi <rishikesh.mhatre@instarem.com>
2021-08-25 08:54:47 +0530

    added: home component main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'employee-payroll-ng-frontend';
+  title: string = 'employee-payroll-ng-frontend';
 }
---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  public employeeCount: number = 10;
+
+  ngOnInit(): void {
+  }
+
+}
